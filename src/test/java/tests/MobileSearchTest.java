33a414d19670b37e786e4374029372365f92fc59
Dinar <dinar-97@mail.ru>
2024-02-21 11:21:24 +0300

    add mobile tests

--- src/test/java/tests/MobileSearchTest.java
+++ src/test/java/tests/MobileSearchTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -11,7 +12,7 @@ import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
-
+@Tag("mobile_test")
 public class MobileSearchTest extends TestBase {
     @Test
     @DisplayName("Проверка выпадающего списка при вводе в поле поиска")
